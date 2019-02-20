b30929fd27c9ad1d7f8a96529eb9c9f1b3772596
DavidSalami <wizdave97@gmail.com>
2019-02-20 16:57:10 +0100

    Enabled generation of Table functionality from height and width values

--- js/app.js
+++ js/app.js
@@ -1,12 +1,45 @@
 let grid=[];
 inputs=document.querySelectorAll('input');
-function getRowColumn(event){
+let table=document.createElement('table');
+let container=document.querySelector('.container');
+function makeGrid(){
+    return new Promise(resolve=>{
+        for(let x=0;x<grid[0];x++){
+            let tr=document.createElement('tr');
+            for(let y=0;y<grid[1];y++){
+                let td=document.createElement('td');
+                tr.appendChild(td)
+            }
+            table.appendChild(tr);
+        }
+        console.log(table)
+        console.log(grid)
+        resolve(table)
     
+    })
+    }
+
+function getRowColumn(event){
     if(event.target.name=='gridHeight'){
         grid[0]=Number(event.target.value);
     }
-    else {grid[1]=Number(event.target.value)}
+    else {grid[1]=Number(event.target.value)
+    }
+    
 }
 for (let input of Array.from(inputs)){
     input.addEventListener('change',getRowColumn)
 }
+
+document.addEventListener('keydown',event=>{
+    if(event.key=='Enter'){
+        if(container.lastElementChild.nodeName=='TABLE') return
+        else{
+            makeGrid().then(table=>{
+                container.appendChild(table)
+            })
+        }
+          
+    }
+})
+
